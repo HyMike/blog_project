148030a830d5bdea9d3df117f45e691dd142c5a0
Michael Hy <hymike88@gmail.com>
2024-02-19 22:24:39 -0800

    adding index, post pages and post templates and css"

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "blog_app/index.html", {})
+
+
+def posts(request):
+    return render(request, "blog_app/posts.html", {})
+
+
+def specific_post(request, slug):
+    return render(request, "blog_app/specific_post.html", {"slug": slug})
