097dc2c35683a87634ae44180d94ce820c288be3
Michael Hy <hymike88@gmail.com>
2024-02-29 21:11:12 -0800

    adding files to make aws elastic beanstalk work

--- blog/blog_app/admin.py
+++ blog/blog_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post, Author, Tag, CommentsModel
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'date', 'author')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('userName', 'post')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(CommentsModel, CommentAdmin)
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,85 +1,140 @@
+from typing import Any
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from datetime import date
+from .models import Post, Author, Tag
+from django.views import View
+from .forms import CommentForm
+from django.views.generic import ListView, DetailView
+from django.urls import reverse
 # Create your views here.
 
-all_posts = [
-    {
-        "slug": "real-estate-investment",
-        "image": 'real_estate.jpg',
-        "author": 'Michael H.',
-        'date': date(2023, 10, 1),
-        'title': 'Investing In Real Estate',
-        'excerpt': 'Investing in real estate presents a compelling opportunity for individuals seeking to build wealth and diversify their investment portfolios',
-        'content': '''
-            Investing in real estate presents a compelling opportunity for individuals seeking to build wealth and diversify their investment portfolios. Unlike traditional investments such as stocks and bonds, real estate offers the unique advantage of providing a tangible asset with the potential for both rental income and long-term appreciation. Whether investing in residential properties, commercial buildings, or vacation rentals, real estate allows investors to generate passive income streams while simultaneously benefiting from property value appreciation over time. Furthermore, real estate investments offer a level of control and autonomy not found in other asset classes, empowering investors to make strategic decisions regarding property management, renovations, and rental pricing to maximize returns.
-            
-            One of the key advantages of real estate investment is its ability to serve as a hedge against inflation and market volatility. Real estate assets have historically demonstrated resilience during economic downturns, providing investors with a stable source of income even in challenging market conditions. Additionally, real estate investments offer tax benefits such as depreciation deductions, mortgage interest deductions, and the ability to defer capital gains taxes through 1031 exchanges. These tax advantages can significantly enhance the overall return on investment and provide investors with additional cash flow to reinvest in their portfolios or allocate towards other financial goals.
 
-            Moreover, real estate investment allows for leveraging the power of other people's money through mortgage financing. With relatively low down payments and favorable interest rates, investors can acquire properties worth significantly more than their initial investment capital. This leverage amplifies returns on investment and accelerates wealth accumulation over time. Additionally, real estate investments provide a level of diversification to an investment portfolio, reducing overall risk and increasing stability. By spreading investment capital across different property types and geographic locations, investors can minimize exposure to market fluctuations and potentially achieve higher risk-adjusted returns. Overall, investing in real estate offers a multifaceted approach to building wealth, providing investors with a tangible asset, passive income, tax advantages, leverage, and diversification benefits.
-        '''
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Michael H.",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+class IndexView(ListView):
+    template_name = 'blog_app/index.html'
+    model = Post
+    ordering = ["-date"]
+    context_object_name = 'latest_posts'
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "nature.jpg",
-        "author": "Michael H.",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-]
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 
 def getPostDate(post):
     return post['date']
 
 
-def index(request):
-    sortedPost = sorted(all_posts, key=getPostDate)
-    getLatestPost = sortedPost[-3:]
-    return render(request, "blog_app/index.html", {'latest_posts': getLatestPost})
+# def index(request):
+#     filterDate = Post.objects.all().order_by('-date')
+
+#     getLatestPost = filterDate[:3]
+#     return render(request, "blog_app/index.html", {'latest_posts': getLatestPost})
 
 
-def posts(request):
-    return render(request, "blog_app/posts.html", {'all_posts': all_posts})
+class AllPostsView(ListView):
+    template_name = 'blog_app/posts.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'all_posts'
 
 
-def specificPost(request, slug):
-    correct_post = next(
-        post for post in all_posts if post['slug'] == slug)
+# def posts(request):
+#     all_posts = Post.objects.all()
+#     return render(request, "blog_app/posts.html", {'all_posts': all_posts})
 
-    return render(request, "blog_app/specific_post.html", {'specific_post': correct_post})
+class SpecificPostView(View):
+    template_name = 'blog_app/specific_post.html'
+    model = Post
+    # context_object_name = 'specific_post'
+
+    def readLater(self, request, post_id):
+        stored_posts = request.session.get('stored_posts')
+
+        if stored_posts is not None:
+            postBoolean = post_id in stored_posts
+        else:
+            postBoolean = False
+
+        return postBoolean
+
+    def get(self, request, slug):
+        correctPost = Post.objects.get(slug=slug)
+        context = {
+            'specific_post': correctPost,
+            'tagCaption': correctPost.tag.all(),
+            'comment_form': CommentForm(),
+            'comments': correctPost.comments.all().order_by('id'),
+            'postBoolean': self.readLater(request, correctPost.id)
+        }
+        return render(request, "blog_app/specific_post.html", context)
+
+    def post(self, request, slug):
+        form = CommentForm(request.POST)
+        correctPost = Post.objects.get(slug=slug)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = correctPost
+            comment.save()
+
+            return HttpResponseRedirect(reverse('specific_post', args=[slug]))
+
+        context = {
+            'specific_post': correctPost,
+            'tagCaption': correctPost.tag.all(),
+            'comment_form': form,
+            'comments': correctPost.comments.all().order_by('id'),
+            'postBoolean': self.readLater(request, correctPost.id)
+
+
+        }
+        return render(request, "blog_app/specific_post.html", context)
+
+
+# def specificPost(request, url_slug):
+#     correctPost = Post.objects.get(slug=url_slug)
+#     tagCaption = correctPost.tag.all()
+
+#     return render(request, "blog_app/specific_post.html", {'specific_post': correctPost,
+#                                                            'tagCaption': tagCaption})
+
+
+# class CommentSection(View):
+#     def post(self, request):
+#         pass
+
+class ReadLaterView(View):
+
+    def get(self, request):
+        stored_posts = request.session.get('stored_posts')
+        context = {}
+        if stored_posts is None or len(stored_posts) == 0:
+            context['posts'] = []
+            context['has_posts'] = True
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context['posts'] = posts
+            context['has_posts'] = False
+
+        return render(request, 'blog_app/stored_post.html', context)
+
+    def post(self, request):
+        stored_posts = request.session.get('stored_posts')
+
+        if stored_posts is None:
+            stored_posts = []
+
+        post_id = int(request.POST['post_id'])
+
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+        else:
+            stored_posts.remove(post_id)
+
+        request.session['stored_posts'] = stored_posts
+
+        return HttpResponseRedirect('/')
+
+        # read_later = request.POST['read_later']
